2a43c7cdd9dfc9a50fd303bb9fdf8dcff2bb6717
Outvi V <git@outv.im>
2020-07-26 21:54:20 +0800

    feat: display links for tablet/mobile

--- source/js/post.js
+++ source/js/post.js
@@ -44,3 +44,24 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 })
+
+// Show url for links
+if (
+  !new URL(window.location).searchParams.get('hideurl') &&
+  (window.innerWidth || 1024) < 1024
+) {
+  document
+    .querySelectorAll('article a:not([data-hideurl])')
+    .forEach(function (elem) {
+      let mat = elem.href.match('^([A-Za-z]*://)?([^/]*)/?')
+      if (mat[1] !== 'http://' && mat[1] !== 'https://') return
+      if (mat.length == 3) {
+        let small = document.createElement('small')
+        small.innerText = '(' + mat[2] + ')'
+        small.style.margin = '0 2px'
+        if (mat[1] === 'http://') small.style.color = '#900'
+        small.className = 'sLink'
+        elem.after(small)
+      }
+    })
+}
