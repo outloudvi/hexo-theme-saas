5ead2e13c0bad8be4983bf1b61dbbf2ceb9b17f1
Outvi V <git@outv.im>
2021-03-30 00:56:11 +0800

    feat: improve header UI

--- source/js/headerAnim.js
+++ source/js/headerAnim.js
@@ -1,15 +1,15 @@
 ;(() => {
-  const headerItems = document.querySelector('#headerItems')
+  const headerBar = document.querySelector('#headerBar')
   const progressBar = document.querySelector('#progressBar')
 
-  if (!headerItems || !progressBar) return
+  if (!headerBar || !progressBar) return
 
   function handlerScroll() {
     const scrollY = window.scrollY
     if (scrollY > 180) {
-      headerItems.classList.add('sticky')
+      headerBar.classList.add('sticky')
     } else {
-      headerItems.classList.remove('sticky')
+      headerBar.classList.remove('sticky')
     }
     const perc = window.scrollY / window.scrollMaxY
     progressBar.style.marginRight = `${String((1 - perc) * 100)}vw`
